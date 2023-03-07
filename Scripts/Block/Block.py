41d9f993bc6612630192d2bf99e577b65d0c792a
Patrick Rainsberry <patrick.rainsberry@gmail.com>
2023-03-07 11:07:33 -0800

    Updated extrude in block

--- Scripts/Block/Block.py
+++ Scripts/Block/Block.py
@@ -1,57 +1,69 @@
-#Author-Patrick Rainsberry
-#Description-Basic Script to create a block
+# Author-Patrick Rainsberry
+# Description-Basic Script to create a block
 
-import adsk.core, adsk.fusion, adsk.cam, traceback
+import adsk.cam
+import adsk.core
+import adsk.fusion
+import traceback
+
+
+app = adsk.core.Application.get()
+ui = app.userInterface
+
 
 def run(context):
-    ui = None
     try:
-        app = adsk.core.Application.get()
-        ui  = app.userInterface
-        design = app.activeProduct     
-        
+        #  Get the design
+        #  The "cast" is to enable type hints to work better
+        design = adsk.fusion.Design.cast(app.activeProduct)
+
         # Get reference to the root component
-        rootComp = design.rootComponent
-        
-        #Get reference to the sketchs and plane
-        sketches = rootComp.sketches
-        xyPlane = rootComp.xYConstructionPlane
-        
-        #Create a new sketch and get lines reference
+        root_comp = design.rootComponent
+
+        # Get reference to the sketchs and plane
+        sketches = root_comp.sketches
+        xyPlane = root_comp.xYConstructionPlane
+
+        # Create a new sketch and get lines reference
         sketch = sketches.add(xyPlane)
         lines = sketch.sketchCurves.sketchLines
-        
-        # Use autodesk methods to create input geometry        
+
+        # Use autodesk methods to create input geometry
         point0 = adsk.core.Point3D.create(0, 0, 0)
         point1 = adsk.core.Point3D.create(0, 1, 0)
         point2 = adsk.core.Point3D.create(1, 1, 0)
         point3 = adsk.core.Point3D.create(1, 0, 0)
-        
+
         # Create lines
         lines.addByTwoPoints(point0, point1)
         lines.addByTwoPoints(point1, point2)
         lines.addByTwoPoints(point2, point3)
         lines.addByTwoPoints(point3, point0)
-        
+
         # Get the profile defined by the circle
         profile = sketch.profiles.item(0)
 
+        # Get the extrude features Collection for the root component
+        extrudes = root_comp.features.extrudeFeatures
+
         # Create an extrusion input
-        extrudes = rootComp.features.extrudeFeatures
-        ext_input = extrudes.createInput(profile, adsk.fusion.FeatureOperations.NewBodyFeatureOperation)
-        
+        extrude_input = extrudes.createInput(profile, adsk.fusion.FeatureOperations.NewBodyFeatureOperation)
+
         # Define that the extent is a distance extent of 1 cm
         distance = adsk.core.ValueInput.createByReal(1)
-        
-        # Set the distance extent to be single direction
-        ext_input.setDistanceExtent(False, distance)
-        
-        # Set the extrude to be a solid one
-        ext_input.isSolid = True
+
+        # Define the direction for the extrude feature to be in the positive direction
+        direction = adsk.fusion.ExtentDirections.PositiveExtentDirection
+
+        # Create a distance extent object
+        distance_extent_definition = adsk.fusion.DistanceExtentDefinition.create(distance)
+
+        # Set the extrude feature to be single direction
+        extrude_input.setOneSideExtent(distance_extent_definition, direction)
 
         # Create the extrusion
-        extrudes.add(ext_input)
-        
+        extrudes.add(extrude_input)
+
     except:
         if ui:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
