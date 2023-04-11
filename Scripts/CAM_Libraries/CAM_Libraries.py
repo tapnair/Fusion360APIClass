803aa950da36bd9df16320aa5a9737364248de7c
Patrick Rainsberry <patrick.rainsberry@gmail.com>
2023-04-11 08:46:24 -0700

    Updated CAM Library Script for CAM Part 2

--- Scripts/CAM_Libraries/CAM_Libraries.py
+++ Scripts/CAM_Libraries/CAM_Libraries.py
@@ -12,6 +12,11 @@ def run(context):
     try:
         get_tool()
 
+        app.log(f'Running Query, be patient...')
+        adsk.doEvents()
+
+        library_query()
+
     except:
         if ui:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
@@ -52,3 +57,35 @@ def get_tool():
             return None
 
     return None
+
+
+def library_query():
+    # Get a reference to the CAMManager object.
+    camMgr = adsk.cam.CAMManager.get()
+
+    # Get the ToolLibraries object.
+    toolLibs = camMgr.libraryManager.toolLibraries
+
+    # Create a query object to query the local library.
+    query = toolLibs.createQuery(adsk.cam.LibraryLocations.Fusion360LibraryLocation)
+
+    # Add the search criteria.
+    query.criteria.add('tool_diameter', adsk.core.ValueInput.createByReal(0.25 * 2.54))
+    query.criteria.add('tool_cornerRadius.min', adsk.core.ValueInput.createByReal(0.05 * 2.54))
+    query.criteria.add('tool_cornerRadius.max', adsk.core.ValueInput.createByReal(0.125 * 2.54))
+
+    # Do the query.
+    queryResults = query.query()
+
+    # Get the tool from the results. There can be multiple tools returned. This used the first one.
+    tool = None
+    if len(queryResults) > 0:
+
+        for queryResult in queryResults:
+            tool = queryResult.tool
+
+            description = tool.parameters.itemByName('tool_description').value.value
+            diameter = tool.parameters.itemByName('tool_diameter').value.value
+            app.log(f'Query Result: {description} with diameter {str(diameter)}')
+            adsk.doEvents()
+    return tool
